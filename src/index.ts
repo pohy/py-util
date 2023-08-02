dcc1aaaf8ca97ea4cdbc8ee62d53ebe0c87189e6
pohy <0ronon0@gmail.com>
2023-08-02 17:23:17 +0200

    try compiling with tsc

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@
  * The issue being that Python counts emojis as 1 character, while JS counts them as 2.
  * https://stackoverflow.com/questions/54369513/how-to-count-the-correct-length-of-a-string-with-emojis-in-javascript
  * https://stackoverflow.com/questions/69584227/javascript-vs-python-emoji-length
- * TODO: Similar issue will likely occur during entity insertion in autocomplete
  */
 export function pySlice(text: string, start: number, end?: number) {
+    // yo
     return [...text].slice(start, end).join('');
 }
 
